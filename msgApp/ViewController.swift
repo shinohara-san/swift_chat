edd39e5f665c95711ba9080a9671f072e4229cff
shinohara-san <someiyoshino0809@gmail.com>
2020-02-28 16:15:40 +0900

    Initial Commit

---
+++ msgApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  msgApp
+//
+//  Created by Yuki Shinohara on 2020/02/28.
+//  Copyright © 2020 Yuki Shinohara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
